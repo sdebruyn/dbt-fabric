477d451a996c70696e6539c72e97ef6b82e95a3d
Sam Debruyn <sam@debruyn.dev>
2025-03-29 14:29:00 +0100

    remove skip from ci tests

--- tests/functional/adapter/test_caching.py
+++ tests/functional/adapter/test_caching.py
@@ -13,7 +13,6 @@ class TestCachingLowerCaseModel(BaseCachingLowercaseModel):
     pass
 
 
-@pytest.mark.skip(reason="Fabric DW does not support Case Insensivity.")
 class TestCachingUppercaseModel(BaseCachingUppercaseModel):
     def dbt_profile_target_update(self):
         dwh_name = os.getenv("FABRIC_TEST_DWH_CI_NAME")
