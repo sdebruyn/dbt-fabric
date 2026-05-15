4e33f5878bd44b6c929b96c636b0406e092a202e
Sam Debruyn <sam@debruyn.dev>
2026-05-15 22:44:22 +0200

    Revert "Fix FabricSpark null_compare test (#112)"
    This reverts commit b59495b205bb8e615896d4e402005895929597bb.

--- tests/fabricspark/adapter/utils/test_null_compare.py
+++ tests/fabricspark/adapter/utils/test_null_compare.py
@@ -1,45 +1,9 @@
-import pytest
-
 from dbt.tests.adapter.utils.test_null_compare import BaseMixedNullCompare, BaseNullCompare
 
 
 class TestMixedNullCompareFabricSpark(BaseMixedNullCompare):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "test_mixed_null_compare.yml": """
-version: 2
-models:
-  - name: test_mixed_null_compare
-    data_tests:
-      - assert_equal:
-          actual: actual
-          expected: expected
-""",
-            "test_mixed_null_compare.sql": """
-select
-    1 as actual,
-    cast(null as int) as expected
-""",
-        }
+    pass
 
 
 class TestNullCompareFabricSpark(BaseNullCompare):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "test_null_compare.yml": """
-version: 2
-models:
-  - name: test_null_compare
-    data_tests:
-      - assert_equal:
-          actual: actual
-          expected: expected
-""",
-            "test_null_compare.sql": """
-select
-    cast(null as string) as actual,
-    cast(null as string) as expected
-""",
-        }
+    pass
